642be3e611db135d2d12a09e0c792f37f77d30e7
Yevgeniy Sorokin <ewgenij87snwork@gmail.com>
2021-05-16 21:45:17 +0300

    take labels from Json

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {PointDto} from "../../rest/point.dto";
+import {Chart} from "chart.js";
 
 @Component({
   selector: 'app-chart',
@@ -8,14 +9,32 @@ import {PointDto} from "../../rest/point.dto";
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent implements OnInit {
-  data: PointDto[];
+  dataFromJson: any;
+  chart: Chart;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+  }
 
   ngOnInit(): void {
     this.httpClient.get<PointDto[]>("assets/data.json")
       .subscribe((data) => {
-        this.data = data;
+        const labelsFromJson = data.map(point => point.rating)
+        const uniqueLabels = Array.from(new Set(labelsFromJson));
+
+        this.dataFromJson = {
+          labels: uniqueLabels,
+          datasets: [
+
+          ]
+
+        }
+        console.log(this.dataFromJson)
       })
+
+    // this.chart = new Chart("canvas", {
+    //   type: "line",
+    //   data: this.dataJson,
+    //   options
+    // })
   }
 }
